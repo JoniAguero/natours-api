0a6f29148a4e8eea912a993ca1d30c12505e73ed
Joni <jonathan@eelptex.com>
2020-02-16 14:59:52 +0100

    API Natour: All Endpoints Users

--- app.js
+++ app.js
@@ -85,6 +85,82 @@ const createTour = (req, res) => {
   })
 };
 
+const getUsers = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'TO-DO'
+    }
+  })
+};
+
+const getUserById = (req, res) => {
+  const tour = tours.find(el => el.id === req.params.id)
+  if(!tour) {
+    return res.status(404).json({
+      status: 'Error',
+      data: {
+        tour: 'User not found'
+      }
+    })
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'TO-DO'
+    }
+  })
+};
+
+const patchUser = (req, res) => {
+  const tour = tours.find(el => el.id === req.params.id)
+  if(!tour) {
+    return res.status(404).json({
+      status: 'Error',
+      data: {
+        tour: 'User not found'
+      }
+    })
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'TO-DO'
+    }
+  })
+};
+
+const deleteUser = (req, res) => {
+  const tour = tours.find(el => el.id === req.params.id)
+  if(!tour) {
+    return res.status(404).json({
+      status: 'Error',
+      data: {
+        tour: 'User not found'
+      }
+    })
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'TO-DO'
+    }
+  })
+};
+
+const createUser = (req, res) => {
+  const newId = tours.length + 1;
+  const newUser = Object.assign({ id: newId }, req.body);
+  tours.push(newUser);
+  fs.writeFile(`${__dirname}/dev-data/data/tours.json`, JSON.stringify(tours), err => {
+    res.status
+  })
+  res.status(200).json({
+    status: 'success',
+    message: 'TO-DO'
+  })
+};
+
 app.route('/api/v1/tours')
   .get(getTours)
   .post(createTour)
@@ -93,6 +169,15 @@ app.route('/api/v1/tours/:id')
   .get(getTourById)
   .patch(patchTour)
   .delete(deleteTour)
+
+app.route('/api/v1/users')
+  .get(getUsers)
+  .post(createUser)
+
+app.route('/api/v1/users/:id')
+  .get(getUserById)
+  .patch(patchUser)
+  .delete(deleteUser)
 
 const port = 3000;
 app.listen(port, () => {
