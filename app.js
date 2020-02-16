13169909767639509c4eb5eeef76c7d3c080f587
Joni <jonathan@eelptex.com>
2020-02-16 15:31:17 +0100

    API Natour: Base Structure

--- app.js
+++ app.js
@@ -14,8 +14,4 @@ app.use(express.json());
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
-//START SERVER
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Running on port: ${port}...`)
-});
+module.exports = app;
