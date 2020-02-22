02fad3ce0f8836ce86333f8d4aaff68510426c8e
Joni <jonathan@eelptex.com>
2020-02-22 18:15:24 +0100

    Refactor ApiFeature: filter, sort, limit, paginate.

--- controllers/tours.js
+++ controllers/tours.js
@@ -1,52 +1,16 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
 const Tour = require('../models/tour.model');
+const APIFeatures = require('../utils/apiFeatures');
 
 const getTours = async (req, res) => {
   try {
-    // Filtering
-    const queryFilters = { ...req.query };
-    const exludeFields = ['page', 'sort', 'limit', 'fields'];
-    exludeFields.forEach(elem => delete queryFilters[elem]);
-
-    // Advances Filtering
-    let queryStr = JSON.stringify(queryFilters);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // Sorting
-
-    if(req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt')
-    }
-
-    // Field Limiting
-
-    if(req.query.fields) {
-      const fieldBy = req.query.fields.split(',').join(' ');
-      query = query.select(fieldBy);
-    } else {
-      query = query.select('-__v')
-    }
-
-    // Pagination
-
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if(req.query.page) {
-      const lengthTours = await Tour.countDocuments();
-      if(skip > lengthTours) throw new Error('Limit superated')
-    }
-
-    // Execute Query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'success',
