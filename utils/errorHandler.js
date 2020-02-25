a953882ed55b077632297bb1b7f8f254918819d7
Joni <jonathan@eelptex.com>
2020-02-25 19:29:59 +0100

    Handling Invalid Database IDs

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,9 +1,51 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+/* eslint-disable node/no-unsupported-features/es-syntax */
+const AppError = require('./appError');
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+module.exports = (err, req, res, next) => {
+
+  const handleCastErrorDB = (error) => {
+    console.log(error);
+    
+    const message = `Invalid ${error.path}: ${error.value}.`;
+    return new AppError(message, 400);
+  }
+
+  const errorDev = () => {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    });
+  }
+
+  const errorProd = () => {
+    if(err.isOperational) {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+    } else {
+      console.error('ERROR ', err)
+      res.status(err.statusCode).json({
+        status: 'error',
+        message: 'Something went wrong!'
+      });
+    }
+  }
+
+  const handleErrorDB = fn => {
+    const error = { ...err };
+    if(error.name === 'CastError') {
+      handleCastErrorDB(error);
+    } else {
+      fn();
+    } 
+  }
+
+  if(process.env.NODE_ENV === 'development') {
+    handleErrorDB(errorDev);
+  } else if(process.env.NODE_ENV === 'production') {
+    handleErrorDB(errorProd);
+  }
 }
