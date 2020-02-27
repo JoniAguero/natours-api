12ae56d32b021e7fd2bf5e2981bb857dc44f905e
Joni <jonathan@eelptex.com>
2020-02-27 18:19:44 +0100

    Validator User creation

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
-const emailSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A user must have a name'],
@@ -25,9 +26,22 @@ const emailSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your password']
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!'
+    }
   }
 });
 
-const User = mongoose.model('User', emailSchema);
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+})
+
+const User = mongoose.model('User', userSchema);
+
 module.exports = User;
