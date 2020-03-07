702958c49a6bd564a8b5c7a68eda167dde1459ec
Joni <jonathan@eelptex.com>
2020-03-07 08:24:48 +0100

    Update the current user: Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,6 +13,18 @@ const signToken = id => {
   })
 }
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  res.status(statusCode).json({
+    status: 'sucess',
+    token,
+    data: {
+      user
+    }
+  })
+}
+
 const signup = catchAsync(async (req, res, next) => {
 
   const newUser = await User.create({
@@ -22,15 +34,7 @@ const signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm
   });
 
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
-    status: 'sucess',
-    token,
-    data: {
-      user: newUser
-    }
-  })
+  createSendToken(newUser, 201, res);
 });
 
 const login = catchAsync(async (req, res, next) => {
@@ -50,11 +54,7 @@ const login = catchAsync(async (req, res, next) => {
   }
 
   // 3) If everything oj, send token to client
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token
-  })
+  createSendToken(user, 200, res);
 
 });
 
@@ -179,13 +179,23 @@ const resetPassword = catchAsync(async (req, res, next) => {
   // 3) Update changedPasswordAt property for the user
   // 4) Log the user in, send JWT
 
-  const token = signToken(user._id);
+  createSendToken(user, 200, res);
 
-  res.status(200).json({
-    status: 'sucess',
-    token
-  });
+});
 
+const updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from collection
+  const user = await User.findById(req.user.id).select('+password');
+  // 2) Check if posted currend password is correct
+  if(!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong.', 401));
+  }
+  // 3) If so, update password
+  user.password = req.body.password;
+  user.passwordCurrent = req.body.passwordCurrent;
+  await user.save();
+  // 4) Log user in, send JWT
+  createSendToken(user, 200, res);
 });
 
 module.exports = {
@@ -194,5 +204,6 @@ module.exports = {
   protect,
   restricTo,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  updatePassword
 };
--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,6 @@ router.post('/login', controller.login);
 
 router.post('/forgot-password', controller.forgotPassword);
 router.patch('/reset-passwor/:token', controller.resetPassword);
+router.patch('/update-password/:token', controller.protect, controller.updatePassword);
 
 module.exports = router;
