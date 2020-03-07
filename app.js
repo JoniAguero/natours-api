7abe5539b741fa2750cce4032f3426941ab75389
Joni <jonathan@eelptex.com>
2020-03-07 11:01:02 +0100

    Security: Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 
 const AppError = require('./utils/appError');
 const errorHandler = require('./utils/errorHandler');
@@ -32,6 +35,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body only into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitize against XSS
+app.use(xss())
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
