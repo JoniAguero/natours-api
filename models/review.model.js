25337a85fe8f508a637f7b3a7f1d89fe0a047cd7
Joni <jonathan@eelptex.com>
2020-03-11 12:29:01 +0100

    Review Schema

---
+++ models/review.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  review: {
+    type: String,
+    required: [true, 'A review can not be empty!']
+  },
+  rating: {
+    type: Number,
+    required: [true, 'A tour must have a rating'],
+    mix: 1,
+    max: 5
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Review must belong to a tour.']
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Review must belong to a user.']
+  }
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
