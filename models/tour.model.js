ca7a6c0479005c966b8a2bc2183e49cebc367c57
Joni <jonathan@eelptex.com>
2020-03-11 11:26:25 +0100

    Modelling Tour Guides: Embedding

--- models/tour.model.js
+++ models/tour.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./user.model')
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -111,6 +112,13 @@ tourSchema.pre('save', function(next){
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+tourSchema.pre('save', async function(next){
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
+
 
 // tourSchema.post('save', function(doc, next){
 //   console.log(doc);
